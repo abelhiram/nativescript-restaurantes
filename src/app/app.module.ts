f31c144f98534ac05205c8dbe9294d0bd567b54a
abelhiram <52177915+abelhiram@users.noreply.github.com>
2019-06-25 11:37:17 -0700

    proyecto funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from './login/login.component';
-import { Prueba1Component } from './prueba1/prueba1.component';
 
 @NgModule({
     bootstrap: [
@@ -16,8 +14,6 @@ import { Prueba1Component } from './prueba1/prueba1.component';
     ],
     declarations: [
         AppComponent,
-        LoginComponent,
-        Prueba1Component
     ],
     schemas: [
         NO_ERRORS_SCHEMA
